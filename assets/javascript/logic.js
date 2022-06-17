9c427f00a59bd1bfaa6dedb17e206b2ea5a8ee74
samvrny <samvrny@gmail.com>
2022-06-17 18:44:15 -0500

    restructure HTML text and CSS and added initial JS variables/text

---
+++ assets/javascript/logic.js
@@ -0,0 +1,10 @@
+var quizBody = document.querySelector("#apple");
+var header = document.querySelector("#quiz-header");
+var buttons = document.querySelector("#buttons-div");
+
+//Initial screen when page loads up
+
+var quizHeader = document.createElement("h2");
+var quizButtons = document.createElement("button");
+
+quizHeader.innerHTML = ""
