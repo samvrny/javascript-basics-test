c93897e5ac7f9874a907ed619f7aaaad89dee632
samvrny <samvrny@gmail.com>
2022-06-17 18:57:44 -0500

    got initial page to load correctly

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,10 +1,14 @@
-var quizBody = document.querySelector("#apple");
+var body = document.querySelector("#apple");
 var header = document.querySelector("#quiz-header");
 var buttons = document.querySelector("#buttons-div");
 
 //Initial screen when page loads up
 
-var quizHeader = document.createElement("h2");
-var quizButtons = document.createElement("button");
+var quizHeader = document.createElement("div");
+var quizButtons = document.createElement("div");
 
-quizHeader.innerHTML = ""
+quizHeader.innerHTML = "<h2 class = 'quiz-titles'>Click here to start the quiz!</h3>";
+quizButtons.innerHTML ="<button class = 'buttons'>Begin!</button>";
+
+body.appendChild(quizHeader);
+body.appendChild(quizButtons);
