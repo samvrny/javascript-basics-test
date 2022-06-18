0883d8544fa3505049c0aaf1f667a5ce4002556b
samvrny <samvrny@gmail.com>
2022-06-17 19:47:57 -0500

    made the first questions header appear once begin button is pressed

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,6 +1,7 @@
-var body = document.querySelector("#apple");
+var form = document.querySelector("#apple");
 var header = document.querySelector("#quiz-header");
 var buttons = document.querySelector("#buttons-div");
+var questionsArr = [];
 
 //Initial screen when page loads up
 
@@ -10,7 +11,20 @@ var quizButtons = document.createElement("div");
 quizHeader.innerHTML = "<h2 class = 'quiz-titles'>Click here to start the quiz!</h3>";
 quizButtons.innerHTML ="<button class = 'buttons'>Begin!</button>";
 
-body.appendChild(quizHeader);
-body.appendChild(quizButtons);
+form.appendChild(quizHeader);
+form.appendChild(quizButtons);
+
+var firstQuestion = function(event) {  //should I add event in the parenthsis for this? Why would that even be there?
+    event.preventDefault();
+    quizHeader.remove();
+    quizButtons.remove();
+
+    var question1 = document.createElement("div");
+    
+
+    question1.innerHTML = "<h2 class = 'quiz-titles'>What does DOM stand for?</h2>";
+
+    form.appendChild(question1);
+};
 
 quizButtons.addEventListener("click", firstQuestion);
